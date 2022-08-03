8584a478865c1a605a0e970c4868465e076ad86c
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-08-03 19:11:12 +0200

    Fixed tests for substract, multiply and divicer

--- app/__tests__/basic-operations.test.js
+++ app/__tests__/basic-operations.test.js
@@ -22,9 +22,9 @@ describe("Test subtract operator", () => {
     expect(subtract(10, 0)).toBe(10);
     expect(subtract(10, 10)).toBe(0);
     expect(subtract(10, 10)).not.toBe(1);
-    expect(() => sum(10)).toThrowError("This function expect 2 numbers");
-    expect(() => sum()).toThrowError("This function expect 2 numbers");
-    expect(() => sum("a", "b")).toThrowError("This function expect 2 numbers");
+    expect(() => subtract(10)).toThrowError("This function expect 2 numbers");
+    expect(() => subtract()).toThrowError("This function expect 2 numbers");
+    expect(() => subtract("a", "b")).toThrowError("This function expect 2 numbers");
   });
 });
 
@@ -34,9 +34,9 @@ describe("Test multiply operator", () => {
     expect(multiply(10, 0)).toBe(0);
     expect(multiply(10, 10)).toBe(100);
     expect(multiply(10, 10)).not.toBe(1);
-    expect(() => sum(10)).toThrowError("This function expect 2 numbers");
-    expect(() => sum()).toThrowError("This function expect 2 numbers");
-    expect(() => sum("a", "b")).toThrowError("This function expect 2 numbers");
+    expect(() => multiply(10)).toThrowError("This function expect 2 numbers");
+    expect(() => multiply()).toThrowError("This function expect 2 numbers");
+    expect(() => multiply("a", "b")).toThrowError("This function expect 2 numbers");
   });
 });
 
@@ -46,8 +46,8 @@ describe("Test divide operator", () => {
     expect(divide(10, 0)).toBe(Infinity);
     expect(divide(10, 10)).toBe(1);
     expect(divide(10, 10)).not.toBe(10);
-    expect(() => sum(10)).toThrowError("This function expect 2 numbers");
-    expect(() => sum()).toThrowError("This function expect 2 numbers");
-    expect(() => sum("a", "b")).toThrowError("This function expect 2 numbers");
+    expect(() => divide(10)).toThrowError("This function expect 2 numbers");
+    expect(() => divide()).toThrowError("This function expect 2 numbers");
+    expect(() => divide("a", "b")).toThrowError("This function expect 2 numbers");
   });
 });
