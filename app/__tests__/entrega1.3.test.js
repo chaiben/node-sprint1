eddbf4b3272645b78bf0096199cc19b14249099c
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-08-03 18:01:42 +0200

    changed coverage

--- app/__tests__/entrega1.3.test.js
+++ app/__tests__/entrega1.3.test.js
@@ -1,23 +1,32 @@
-const { describe, expect } = require("@jest/globals");
+const { describe, expect, beforeAll } = require("@jest/globals");
 
 const { sayHello } = require("../entrega1.3");
 const { talk } = require("../entrega1.3");
 
-describe("Entrega 1.3", () => {
-  const logSpy = jest.spyOn(console, "log");
+const logSpy = jest.spyOn(console, "log");
 
+describe("Entrega 1.3 - sayHello funcion", () => {
   it("Test sayHello function", () => {
     sayHello("Marçal");
     expect(logSpy).toHaveBeenCalledWith("N1E2 response: Hello Marçal");
   });
-  it("Test sayHello function without param", () => {
+
+  it("Test sayHello function input param", () => {
     expect(() => sayHello()).toThrowError(
       'This function expects to receive a "name" string as parameter'
     );
   });
+});
 
+describe("Entrega 1.3 - talk function", () => {
   it("Test talk function", () => {
     talk("Rosa", sayHello);
     expect(logSpy).toHaveBeenCalledWith("N1E2 response: Hello Rosa");
+  });
+
+  it("Test talk function input param", () => {
+    expect(() => talk()).toThrowError(
+      'This function expects to receive a "name" string and a "callback" function as parameters'
+    );
   });
 });
