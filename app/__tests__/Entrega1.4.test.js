8c1bff4bc5e37001f14701ef9655591e8961d18e
Marçal <chaiben79@gmail.com>
2022-09-07 10:31:30 +0200

    Entrega 1.6 - Nivel 1 completo

--- app/__tests__/Entrega1.4.test.js
+++ app/__tests__/Entrega1.4.test.js
@@ -3,8 +3,18 @@ const { describe, expect } = require("@jest/globals");
 const { callDelay } = require("../Entrega1.4");
 
 describe("callDelay", () => {
-  it("Check promise function", async () => {
+  // There are two different methods to test an asyncronous function
+
+  it("Method 1: Check promise function", async () => {
     const data = await callDelay()
     expect(data).toBe('Resolved');
   });
+
+  it("Method 2: Check promise function", async () => {
+    callDelay().then(result =>{
+      expect(result).toBe('Resolved');
+    })
+  });
+
+  // 
 });
