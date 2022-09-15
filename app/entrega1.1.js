91262585d407fd80cc57f625df92accbb7d869a4
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-09-15 17:38:08 +0200

    Fixed Entrega 1.1

--- app/entrega1.1.js
+++ app/entrega1.1.js
@@ -14,27 +14,30 @@ const lastName = 'Chaiben'
 console.log(`${firstName} ${lastName}`)
 
 // Nivel 2 - Ejercicio 2
+// Invoca una funció que retorni un valor des de dins d'una template literal.
+const returnName = (name) => name
 console.log('Nivel 2 - Ejercicio 2')
 const fullName = (firstName, lastName) =>
-  console.log(`${firstName} ${lastName}`)
+  console.log(`${returnName(firstName)} ${returnName(lastName)}`)
 fullName('Marçal', 'Chaiben')
 
 // Nivel 3 - Ejercicio 1
 console.log('Nivel 3 - Ejercicio 1')
-const functionArray = [
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } },
-  () => { for (let i = 0; i < 10; i++) { console.log(i) } }
-]
-functionArray.forEach(func => func())
+const functionArray = () => {
+  let result = []
+  for (let y = 0; y < 10; y++) {
+    result = [...result,
+      () => { for (let i = 0; i < 10; i++) { console.log(i) } }
+    ]
+  }
+  return result
+}
+functionArray().forEach(func => func())
 
 // Nivel 3 - Ejercicio 2
-console.log('Nivel 3 - Ejercicio 2');
-((nom) => console.log(nom))('Marçal')
+console.log('Nivel 3 - Ejercicio 2')
+const variable = ((nom) => {
+  console.log(nom)
+  return nom
+})('Hello Marçal')
+console.log('Print variable result: ', variable)
